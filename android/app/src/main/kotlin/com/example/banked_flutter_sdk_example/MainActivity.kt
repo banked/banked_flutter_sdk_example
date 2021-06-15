05f20d59f7be65f81dfad97d44cf24d682f14e76
Ross Bines <ross@rbinesconsulting.co.uk>
2021-06-15 15:13:17 +0100

    Initial code commit

---
+++ android/app/src/main/kotlin/com/example/banked_flutter_sdk_example/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.banked_flutter_sdk_example
+
+import android.os.Bundle
+import com.banked.checkout.Banked
+import com.banked.checkout.OnPaymentSessionListener
+import com.banked.checkout.feature.status.model.PaymentResult
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+private const val CHANNEL = "com.example.banked_flutter_sdk_example/banked_sdk"
+private const val BANKED_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+class MainActivity : FlutterFragmentActivity(), OnPaymentSessionListener {
+
+    private var methodChannel: MethodChannel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Banked.onPaymentSessionListener = this
+        Banked.apiKey = BANKED_API_KEY
+    }
+
+    override fun onDestroy() {
+        Banked.onPaymentSessionListener = null
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Banked.onStart(activity = this)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        if (methodChannel == null) {
+            methodChannel = MethodChannel(
+                flutterEngine.dartExecutor.binaryMessenger,
+                CHANNEL
+            )
+
+            methodChannel?.setMethodCallHandler { call, result ->
+                if (call.method == "startSdk") {
+                    val arguments = call.arguments as Map<String, Any>
+                    val paymentId = arguments["payment id"]?.toString() ?: "XXXXXXXX"
+                    val continueUrl = arguments["continue url"]?.toString() ?: "XXXXXXXX"
+
+                    Banked.startPayment(
+                        activity = this,
+                        paymentId = paymentId,
+                        continueUrl = continueUrl
+                    )
+                    result.success("SDK Started")
+                } else {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    override fun onPaymentFailed(paymentResult: PaymentResult) {
+        methodChannel?.invokeMethod("BankedSdkPaymentFailed", toParameterMap(paymentResult))
+    }
+
+    override fun onPaymentSuccess(paymentResult: PaymentResult) {
+        methodChannel?.invokeMethod("BankedSdkPaymentSuccess", toParameterMap(paymentResult))
+    }
+
+    private fun toParameterMap(paymentResult: PaymentResult): Map<String, String> {
+        return mapOf(
+            "paymentId" to paymentResult.paymentId,
+            "amountFormatted" to paymentResult.amountFormatted,
+            "providerName" to paymentResult.providerName,
+            "payeeName" to paymentResult.payeeName
+        )
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const methodChannel =
+      const MethodChannel('com.example.banked_flutter_sdk_example/banked_sdk');
+  String _sdkStatus = 'Click button to start the SDK';
+
+  _MyHomePageState() {
+    methodChannel.setMethodCallHandler((call) => _processSdkCallback(call));
+  }
+
+  Future<dynamic> _processSdkCallback(MethodCall call) async {
+    print("Call - " + call.method);
+
+    String newStatus = "Unknown";
+
+    if (call.method == "BankedSdkPaymentSuccess") {
+      newStatus = _buildPaymentStatusText("Payment Success", call);
+    } else if (call.method == "BankedSdkPaymentSuccess") {
+      newStatus = _buildPaymentStatusText("Payment Failed", call);
+    }
+
+    setState(() {
+      _sdkStatus = newStatus;
+    });
+  }
+
+  String _buildPaymentStatusText(String prefixStatus, MethodCall call) {
+    return prefixStatus +
+        "\nProvider Name: " +
+        call.arguments["providerName"] +
+        "\nPayee Name: " +
+        call.arguments["payeeName"] +
+        "\nAmount Formatted: " +
+        call.arguments["amountFormatted"];
+  }
+
+  Future<void> _startBankedSdk() async {
+    String sdkStatus;
+    try {
+      final Map params = <String, dynamic>{
+        'payment id': "XXXXXXXX-XXXX-XXXX-XXXXX-XXXXXXXXXXX",
+        'continue url': "XXXXXXXXXXX",
+      };
+
+      await methodChannel.invokeMethod('startSdk', params);
+      sdkStatus = "Waiting for SDK result";
+    } on PlatformException catch (e) {
+      sdkStatus = "Failed to get start Banked SDK: '${e.message}'.";
+    }
+    setState(() {
+      _sdkStatus = sdkStatus;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: _startBankedSdk,
+              child: Text('Start Banked SDK'),
+            ),
+            Text(_sdkStatus)
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
